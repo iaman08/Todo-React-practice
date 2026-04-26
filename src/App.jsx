57d57ecc9e4be6995c191448c561a89d83b4bf88
iaman08 <amanraj.cs@yahoo.com>
2026-04-26 09:35:28 +0530

    added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,83 @@
-import { useState, useEffect, useRef, useCallback, useMemo, memo } from "react";
-
+import {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useMemo,
+  memo,
+  useContext,
+  createContext
+} from "react";
 import "./App.css";
 
+const TaskCard = memo(function TaskCard({
+  task,
+  editId,
+  editValue,
+  editPriority,
+  inputValue,
+  search,
+  onEdit,
+  onDelete,
+  onSave,
+  setEditValue,
+  setEditPriority,
+}) {
+  return (
+    <div
+      style={{
+        padding: "10px",
+        marginBottom: "8px",
+        border: "1px solid #ccc",
+        borderRadius: "8px",
+      }}
+    >
+ 
+      { editId === task.id?(
+        <>
+          <input value={editValue} onChange={(e) => setEditValue(e.target.value)}/>
+          <select value={editPriority} onChange={(e) => setEditPriority(e.target.value)}>
+             <option value="low">Low</option>
+             <option value="Medium">Medium</option>
+             <option value="High">High</option>
+          </select>
 
+          <button onClick={() => onSave(task.id, editValue, editPriority)}>
+            Save </button>
+        </>
+      ):(
+        <>
+          <span>{task.title}</span>
+          <span style={{ marginLeft: "8px", fontSize: "12px", color: "gray"}}>
+            [{ task.priority}]
+          </span>
+          <button onClick={()=> onEdit(task)}>Edit</button>
+        </>
+      )}
+
+      <button onClick={() => onDelete(task.id)} style={{marginLeft: "12px", color: "red" }}>
+        DELETE
+      </button>
+
+    </div>
+  );
+});
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({children}){
+    const [theme, setTheme] = useState("light")
+
+    const toggleTheme = useCallback(()=>{
+      setTheme(prev => prev === "light" ? "dark" : "light")
+    },[])
+
+    return(
+      <ThemeContext.Provider value={{ theme, toggleTheme}}>
+          {children}
+      </ThemeContext.Provider>
+    )
+}
 
 function App() {
   const [tasks, setTasks] = useState(() => {
@@ -48,22 +123,46 @@ function App() {
     [tasks],
   );
 
-  const updateTask = useCallback((id, newTitle, newPriority) => {
-     setTasks(tasks.map((task)=>{
-        return task.id === id?{ ...task, title: newTitle, priority: newPriority } : task
-     }))
-  },[tasks]);
-
+  const updateTask = useCallback(
+    (id, newTitle, newPriority) => {
+      setTasks(
+        tasks.map((task) => {
+          return task.id === id
+            ? { ...task, title: newTitle, priority: newPriority }
+            : task;
+        }),
+      );
+    },
+    [tasks],
+  );
 
   const [editId, setEditId] = useState();
-  const [editValue,setEditValue] = useState("");
+  const [editValue, setEditValue] = useState("");
   const [editPriority, setEditPriority] = useState("medium");
+  const { theme, toggleTheme} = useContext(ThemeContext);
+
+  const handleEdit = useCallback((task)=>{
+    setEditId(task.id);
+    setEditValue(task.title);
+    setEditPriority(task.priority);
+  },[]);
+
+ const handleSave = useCallback((id, newTitle,newPriority)=>{
+  updateTask(id, newTitle, newPriority);
+  setEditId(null)
+ },[updateTask])
 
   return (
     <>
-      <div style={{ padding: "24px", fontFamily: "sans-serif" }}>
-        <h1>⚡Kanban Board</h1>
+      <div style={{ padding: "24px",
+       fontFamily: "sans-serif",
+       minHeight: "100vh",
+       background: theme ==="dark"? "#ff1b67" : "#ffffff",
+       color: theme === "dark"? "#ffffff" : "#000000" }}>
 
+      
+        <h1>⚡Kanban Board</h1>
+        <button onClick={toggleTheme}>{ theme === "light" ?"DARK" : "Light" }</button>
         <input
           value={search}
           onChange={(e) => {
@@ -83,62 +182,27 @@ function App() {
           />
           <button onClick={addTask}>ADD TASK</button>
         </div>
-           
+
+        {/* search button */}
 
 
-
-            {/* search button */}
-
-        {filteredTasks.map((task) => (
-          <div
-            key={task.id}
-            style={{
-              padding: "10px",
-              marginBottom: "8px",
-              border: "1px solid #ccc",
-              borderRadius: "8px",
-            }}
-          >
-            
-            { editId === task.id ?(
-              <>
-                <input value={editValue} onChange={(e)=> setEditValue(e.target.value)} />
-                <select value={editPriority} onChange={(e)=> setEditPriority(e.target.value)}>
-                   <option value="low">Low</option>
-                   <option value="medium">Medium</option>
-                   <option value="high">High</option>
-                 </select>
-                  <button onClick={()=>{
-                    updateTask(task.id, editValue, editPriority)
-                    setEditId(null)
-                  }}>Save</button>
-                  
-              </>
-            ):(
-              <>
-                <span>{task.title}</span>
-                <button onClick={()=>{
-                  setEditId(task.id)
-                  setEditValue(task.title)
-                  setEditPriority(task.priority)
-                }}>Edit</button>
-              </>
-            )}
-
-            
-            <button onClick={() => deleteTask(task.id)}
-              style={{ marginLeft: "12px", color: "red" }}
-            >
-              {" "}
-              DELETE
-            </button>
-          </div>
-        ))}
-
-       
+          { filteredTasks.map((task)=>(
+            <TaskCard 
+              key={task.id}
+              task={task}
+              editValue={editValue}
+              editId={editId}
+              editPriority={editPriority}
+              onDelete={deleteTask}
+              onEdit={handleEdit}
+              onSave={handleSave}
+              setEditValue={setEditValue}
+              setEditPriority={setEditPriority}
+              />
+          ))}
       </div>
     </>
-  );
+  )
 }
 
 export default App;
