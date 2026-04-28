efd8b20afdc24a0fc8d5d09c39a835764d1defc2
iaman08 <amanraj.cs@yahoo.com>
2026-04-28 22:44:50 +0530

    fix: added react project properly

---
+++ react/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css' 
+import App,{ThemeProvider} from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider>  
+        <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
